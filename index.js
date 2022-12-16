154d9986b5fe2929445125b7fa687e5e3a0c8b25
cheesezzzz <huyng211203@gmail.com>
2022-12-16 20:55:05 +0700

    latest commit

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const showMenu = document.getElementById("menu");
 const menuBtn = document.querySelector(".hamburger-button");
 const body = document.getElementById("body");
 
-menuBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    showMenu.classList.toggle("opacity-0");
-    showMenu.classList.toggle("invisible");
-    body.classList.toggle("overflow-hidden");
+menuBtn.addEventListener("click", function () {
+    if (showMenu.classList.contains("opacity-0")) {
+        showMenu.classList.remove("opacity-0")
+        showMenu.classList.add("opacity-100")
+        showMenu.classList.remove("invisible")
+        body.classList.add('overflow-hidden')
+    } else if(showMenu.classList.contains("opacity-100")) {
+        showMenu.classList.remove("opacity-100")
+        showMenu.classList.add("opacity-0")
+        showMenu.classList.remove("visible")
+        body.classList.remove('overflow-hidden')
+    }
 });
 
